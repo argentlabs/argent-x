a02444e788161a46e0f86fcd14a951d63edffaa4
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2021-11-09 09:44:34 +0100

    Using bracket notation on window object to avoid compile errors

--- packages/connect/src/index.ts
+++ packages/connect/src/index.ts
@@ -1,8 +1,8 @@
 import App from "./App.svelte"
 
 const getStarknet = () => {
-  if (window.starknet) {
-    return window.starknet
+  if (window["starknet"]) {
+    return window["starknet"]
   } else {
     console.log("no starknet found in window")
     new App({ target: document.body })
