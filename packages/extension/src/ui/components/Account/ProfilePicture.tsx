9d23c1c2bafc1416c1dec5364620b072e0dcbdb7
graemeblackwood <graeme84@gmail.com>
2021-12-01 18:51:32 +0000

    [UPDATE] Adds hover state to profile button.

--- packages/extension/src/ui/components/Account/ProfilePicture.tsx
+++ packages/extension/src/ui/components/Account/ProfilePicture.tsx
@@ -4,7 +4,13 @@ export const ProfilePicture = styled.img`
   width: 36px;
   height: 36px;
   flex: 0 0 36px;
-  border-radius: 18px;
-
+  border-radius: 50%;
+  border: 2px solid transparent;
   cursor: pointer;
+  
+  &:hover,
+  &:focus {
+    border: 2px solid transparent;
+    outline: 2px solid white;
+  }
 `
