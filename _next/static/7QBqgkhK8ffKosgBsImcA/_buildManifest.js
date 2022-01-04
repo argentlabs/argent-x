36717ff6115c3035145352435ef7c061aebd6b9c
janek26 <janek26@users.noreply.github.com>
2022-01-04 13:11:43 +0000

    deploy: 975ab54fa92de17218780ac2f7b89f9e5e98ad8d

--- _next/static/7QBqgkhK8ffKosgBsImcA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/567-740834af70cbdb02.js","static/css/4c4d43a419b0d951.css","static/chunks/pages/index-5dcfb46b111974e7.js"],"/_error":["static/chunks/pages/_error-d20da8f9a50c916f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
