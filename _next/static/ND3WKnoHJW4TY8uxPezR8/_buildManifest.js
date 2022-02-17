004d66dfde043bb6bbabe86c6f93d9e6b198e9db
delaaxe <delaaxe@users.noreply.github.com>
2022-02-17 10:14:52 +0000

    deploy: fd26472d7b0d124edd4bd950b8e9d541536c90b3

--- _next/static/ND3WKnoHJW4TY8uxPezR8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/567-2d55cdaff03931cc.js","static/css/d080defbe42cdd35.css","static/chunks/pages/index-b693b1f8e0ac5e67.js"],"/_error":["static/chunks/pages/_error-d20da8f9a50c916f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
