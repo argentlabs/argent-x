c19ec1a81e785844396710eab4d5902bc296fea5
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-02-24 09:35:09 +0000

    feat(settings): ux improvements

--- packages/extension/src/ui/screens/SettingsScreen.tsx
+++ packages/extension/src/ui/screens/SettingsScreen.tsx
@@ -7,22 +7,45 @@ import { BackButton } from "../components/BackButton"
 import { Button } from "../components/Button"
 import { Header } from "../components/Header"
 import { InputText } from "../components/InputText"
-import { H2, P } from "../components/Typography"
+import { H2 } from "../components/Typography"
 import { routes } from "../routes"
 import { useLocalhostPort } from "../states/localhostPort"
+
+const Title = styled.h3`
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 22px;
+  color: #ffffff;
+`
+
+const P = styled.p`
+  font-size: 15px;
+  color: #8f8e8c;
+  margin-top: 16px;
+`
 
 const SettingsScreenWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 0 32px 24px 32px;
+  padding: 0 0 24px 0;
 
-  ${P} {
-    margin: 16px 0;
+  ${H2} {
+    margin: 0 32px 32px 32px;
   }
 
   ${Button} {
     margin-top: 10px;
   }
+
+  hr {
+    border: none;
+    height: 1px;
+    background-color: #333332;
+  }
+`
+
+const SettingsItem = styled.div`
+  padding: 24px 32px;
 `
 
 const Footer = styled.div`
@@ -42,6 +65,59 @@ export const SettingsScreen: FC = () => {
       </Header>
       <SettingsScreenWrapper>
         <H2>Settings</H2>
+        <SettingsItem>
+          <Title>Lock wallet</Title>
+        </SettingsItem>
+        <hr />
+        <SettingsItem>
+          <Title>Reset dapp connections</Title>
+          <P>
+            Dapps you have previously connected to can auto-connect in the
+            future.
+          </P>
+        </SettingsItem>
+        <hr />
+        <SettingsItem>
+          <Title>Backup file</Title>
+          <P>The backup file contains all your accounts. Keep it secure.</P>
+        </SettingsItem>
+        <hr />
+        <SettingsItem>
+          <Title>Localhost port</Title>
+        </SettingsItem>
+        <hr />
+        <Footer>
+          <P>Help, support &amp; suggestions:</P>
+          <div>
+            <a
+              href="https://discord.com/channels/793094838509764618/908663762150645770"
+              title="Ask a question on the argent-extension channel on StarkNet Discord"
+              target="_blank"
+            >
+              <img
+                src="https://images.prismic.io/argentwebsite/76eac5a3-a4a3-4395-a82a-a3def2438ff0_icon-discord.svg?auto=format%2Ccompress&amp;fit=max&amp;q=50"
+                alt="Argent Discord icon"
+              />
+            </a>
+            <a
+              href="https://github.com/argentlabs/argent-x/issues"
+              title="Post an issue on Argent X GitHub"
+              target="_blank"
+              style={{ marginLeft: 15 }}
+            >
+              <img
+                src="https://images.prismic.io/argentwebsite/460e2528-d9bb-4a47-9339-b72c287c243e_icon-github.svg?auto=format%2Ccompress&amp;fit=max&amp;q=50"
+                alt="Argent Github icon"
+              />
+            </a>
+          </div>
+          <P>Version: v{process.env.VERSION}</P>
+        </Footer>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         <Button
           onClick={() => {
             sendMessage({ type: "STOP_SESSION" })
@@ -85,33 +161,6 @@ export const SettingsScreen: FC = () => {
             useLocalhostPort.setState({ localhostPort: e.target.value })
           }}
         />
-        <Footer>
-          <P>Help, support &amp; suggestions:</P>
-          <div>
-            <a
-              href="https://discord.com/channels/793094838509764618/908663762150645770"
-              title="Ask a question on the argent-extension channel on StarkNet Discord"
-              target="_blank"
-            >
-              <img
-                src="https://images.prismic.io/argentwebsite/76eac5a3-a4a3-4395-a82a-a3def2438ff0_icon-discord.svg?auto=format%2Ccompress&amp;fit=max&amp;q=50"
-                alt="Argent Discord icon"
-              />
-            </a>
-            <a
-              href="https://github.com/argentlabs/argent-x/issues"
-              title="Post an issue on Argent X GitHub"
-              target="_blank"
-              style={{ marginLeft: 15 }}
-            >
-              <img
-                src="https://images.prismic.io/argentwebsite/460e2528-d9bb-4a47-9339-b72c287c243e_icon-github.svg?auto=format%2Ccompress&amp;fit=max&amp;q=50"
-                alt="Argent Github icon"
-              />
-            </a>
-          </div>
-          <P>Version: v{process.env.VERSION}</P>
-        </Footer>
       </SettingsScreenWrapper>
     </>
   )
