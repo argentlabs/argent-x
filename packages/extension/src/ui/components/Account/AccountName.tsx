6255576ddfda3ce2e9651c4319fef83b660a7faa
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-03-10 13:17:00 +0000

    feat(ui): add edit icon next to account title

---
+++ packages/extension/src/ui/components/Account/AccountName.tsx
@@ -0,0 +1,72 @@
+import EditIcon from "@mui/icons-material/Edit"
+import { FC, InputHTMLAttributes, useState } from "react"
+import styled from "styled-components"
+
+import { defaultAccountName } from "../../states/accountMetadata"
+import { InputText } from "../InputText"
+
+const Form = styled.form`
+  display: flex;
+  align-items: center;
+
+  svg {
+    visibility: hidden;
+  }
+
+  &:hover svg {
+    visibility: visible;
+  }
+`
+
+const InputHeader = styled(InputText)`
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 38.4px;
+  margin: 0;
+
+  input {
+    text-align: center;
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 38.4px;
+
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+
+    // allocates space for when bottom-border is shown on focus
+    border-bottom: 1px solid transparent;
+    outline: 0;
+
+    cursor: pointer;
+    &:focus {
+      cursor: text;
+    }
+  }
+`
+
+export const AccountName: FC<InputHTMLAttributes<HTMLInputElement>> = ({
+  value,
+  ...props
+}) => {
+  const [isFocused, setIsFocused] = useState(false)
+  if (isFocused && value === defaultAccountName) {
+    value = ""
+  }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    ;(document.activeElement as any)?.blur()
+  }
+  return (
+    <Form onSubmit={handleSubmit} autoComplete="off">
+      <InputHeader
+        spellCheck="false"
+        value={value}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        {...props}
+      />
+      <EditIcon />
+    </Form>
+  )
+}
