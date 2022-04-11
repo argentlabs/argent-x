ad10c499b8d4f80282ec131666a84f8ecba261a9
Janek Rahrt <janek.rahrt@me.com>
2022-04-11 10:16:35 +0200

    fix: update swr

--- packages/extension/src/ui/App.tsx
+++ packages/extension/src/ui/App.tsx
@@ -60,10 +60,6 @@ const theme = createTheme({
 })
 
 export const App: FC = () => (
-  // TODO: remove this ts-ignore when SWR updates to React 18
-  // this is already the case for SWRs main branch, but it's not released yet. So it should not take too long for them to release the update.
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   <SWRConfig value={{ provider: () => swrCacheProvider }}>
     <ThemeProvider theme={theme}>
       <Suspense fallback={<LoadingScreen />}>
