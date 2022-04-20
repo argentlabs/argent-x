7e19f0db9291c126b6755ff77fed7502ca4830a7
Janek Rahrt <janek.rahrt@me.com>
2022-04-20 13:30:25 +0200

    feat: update upgrade screen

--- packages/extension/src/ui/screens/UpgradeScreen.tsx
+++ packages/extension/src/ui/screens/UpgradeScreen.tsx
@@ -1,14 +1,18 @@
 import { FC, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import styled from "styled-components"
 
-import { BackButton } from "../components/BackButton"
-import { Header } from "../components/Header"
 import { P } from "../components/Typography"
 import { routes } from "../routes"
 import { useAccount } from "../states/account"
 import { useAppState } from "../states/app"
 import { updateWallet } from "../utils/messaging"
 import { ConfirmScreen } from "./ConfirmScreen"
+
+const StyledP = styled(P)`
+  margin-bottom: 16px;
+  line-height: 1.5em;
+`
 
 export const UpgradeScreen: FC = () => {
   const navigate = useNavigate()
@@ -25,29 +29,28 @@ export const UpgradeScreen: FC = () => {
   }
 
   return (
-    <>
-      <Header>
-        <BackButton />
-      </Header>
-      <ConfirmScreen
-        title="Update Wallet"
-        confirmButtonText="Update"
-        rejectButtonText="Cancel"
-        onSubmit={async () => {
-          useAppState.setState({ isLoading: true })
-          await updateWallet(selectedAccount)
-          useAppState.setState({ isLoading: false })
-          navigate(routes.account())
-        }}
-        onReject={() => {
-          navigate(routes.account())
-        }}
-      >
-        <P>
-          You will update your wallet implementation to use the latest features
-          and security.
-        </P>
-      </ConfirmScreen>
-    </>
+    <ConfirmScreen
+      title="Update Wallet"
+      confirmButtonText="Update"
+      rejectButtonText="Cancel"
+      onSubmit={async () => {
+        useAppState.setState({ isLoading: true })
+        await updateWallet(selectedAccount)
+        useAppState.setState({ isLoading: false })
+        navigate(routes.account())
+      }}
+      onReject={() => {
+        navigate(routes.account())
+      }}
+    >
+      <StyledP>
+        You will update your wallet implementation to use the latest features
+        and security.
+      </StyledP>
+      <StyledP>
+        This update is required due to network and account contract changes. We
+        expect these kind of updates to be less frequent as the network matures.
+      </StyledP>
+    </ConfirmScreen>
   )
 }
