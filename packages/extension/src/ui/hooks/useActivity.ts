a305a4af2a85bd8ee6aed8674daa12034861c7e9
Janek Rahrt <janek.rahrt@me.com>
2022-04-22 12:24:53 +0200

    fix: introduce useActivity

---
+++ packages/extension/src/ui/hooks/useActivity.ts
@@ -0,0 +1,27 @@
+import useSWR from "swr"
+
+import { Network } from "../../shared/networks"
+import { fetchActivity } from "../components/Account/accountActivity.service"
+
+interface SWRConfigCommon {
+  suspense?: boolean
+  refreshInterval?: number
+  errorRetryInterval?: number
+}
+
+export function useActivity(
+  address: string,
+  network: Network,
+  config?: SWRConfigCommon,
+) {
+  const { data: activity = {}, ...rest } = useSWR(
+    [address, network, "activity"],
+    fetchActivity,
+    {
+      refreshInterval: 60e3 /* 1 minute */,
+      suspense: true,
+      ...config,
+    },
+  )
+  return { activity, ...rest }
+}
