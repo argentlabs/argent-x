414f5199aff7dc6b41832b82ed37f963c6438c69
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-05-03 15:49:35 +0200

    feat(ui): update disclaimer screen

--- packages/extension/src/ui/screens/DisclaimerScreen.tsx
+++ packages/extension/src/ui/screens/DisclaimerScreen.tsx
@@ -1,4 +1,12 @@
-import { FC } from "react"
+import CircleCheckedFilled from "@mui/icons-material/CheckCircle"
+import CircleUnchecked from "@mui/icons-material/RadioButtonUnchecked"
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+} from "@mui/material"
+import { ChangeEventHandler, FC, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 
@@ -14,12 +22,20 @@ const SP = styled(P)`
 
 export const DisclaimerScreen: FC = () => {
   const navigate = useNavigate()
+  const [conditions, setConditions] = useState({
+    lossOfFunds: false,
+    alphaVersion: false,
+  })
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = ({ target }) =>
+    setConditions({ ...conditions, [target.name]: target.checked })
 
   return (
     <ConfirmScreen
       title="Disclaimer"
-      confirmButtonText="I understand"
+      confirmButtonText="Continue"
       confirmButtonBackgroundColor="#c12026"
+      disableConfirm={!conditions.lossOfFunds || !conditions.alphaVersion}
       singleButton
       onSubmit={() => {
         understandDisclaimer()
@@ -27,10 +43,42 @@ export const DisclaimerScreen: FC = () => {
       }}
     >
       <SP>
-        This is an alpha product for developers. Future versions will likely
-        include breaking changes and you may lose access to your accounts. Do
-        not store valuable assets.
+        StarkNet is in Alpha and may experience technical issues or introduce
+        breaking changes from time to time. Please accept this before
+        continuing.
       </SP>
+      <FormControl component="fieldset" variant="standard">
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={conditions.lossOfFunds}
+                onChange={handleChange}
+                name="lossOfFunds"
+                icon={<CircleUnchecked />}
+                checkedIcon={<CircleCheckedFilled />}
+                color="success"
+              />
+            }
+            label="I understand that StarkNet may introduce changes that make my existing account unusable and force to create new ones."
+            sx={{ mt: 4 }}
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={conditions.alphaVersion}
+                onChange={handleChange}
+                name="alphaVersion"
+                icon={<CircleUnchecked />}
+                checkedIcon={<CircleCheckedFilled />}
+                color="success"
+              />
+            }
+            label="I understand that StarkNet may experience performance issues and my transactions may fail for various reasons."
+            sx={{ mt: 4 }}
+          />
+        </FormGroup>
+      </FormControl>
     </ConfirmScreen>
   )
 }
