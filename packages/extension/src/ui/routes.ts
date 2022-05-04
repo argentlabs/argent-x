848360210e306e59030dfb40897f6d7b98550388
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-05-04 16:46:09 +0200

    refactor(routes): make it easier to use params

--- packages/extension/src/ui/routes.ts
+++ packages/extension/src/ui/routes.ts
@@ -1,37 +1,57 @@
+import { isString } from "lodash-es"
+
+const route = <T extends (..._: any[]) => string>(
+  ...[value, path]: [string] | [T, string]
+): T & { path: string } => {
+  if (isString(value)) {
+    return Object.defineProperty((() => value) as any, "path", { value })
+  }
+  return Object.defineProperty(value as any, "path", { value: path })
+}
+
 export const routes = {
-  welcome: () => "/index.html",
-  newWallet: () => "/wallets/new",
-  backupRecovery: () => "/recover/backup",
-  seedRecovery: () => "/recover/seed",
-  seedRecoveryPassword: () => "/recover/seed/password",
-  setupRecovery: () => "/recovery",
-  setupSeedRecovery: () => "/recovery/seed",
-  confirmSeedRecovery: () => "/recovery/seed/confirm",
-  lockScreen: () => "/lock-screen",
-  accountTokens: () => "/account/tokens",
-  accountNfts: () => "/account/nfts",
-  accountActivity: () => "/account/activity",
-  accountNft: (contractAddress: string, tokenId: string) =>
-    `/account/nfts/${contractAddress}/${tokenId}`,
-  accountNftPath: () => `/account/nfts/:contractAddress/:tokenId`,
-  upgrade: () => "/account/upgrade",
-  accounts: () => "/accounts",
-  newToken: () => "/tokens/new",
-  token: (tokenAddress: string) => `/tokens/${tokenAddress}`,
-  tokenPath: () => "/tokens/:tokenAddress",
-  hideToken: (tokenAddress: string) => `/tokens/${tokenAddress}/hide`,
-  hideTokenPath: () => "/tokens/:tokenAddress/hide",
-  reset: () => "/reset",
-  disclaimer: () => "/disclaimer",
-  legacy: () => "/legacy",
-  settings: () => "/settings",
-  settingsNetworks: () => "/settings/networks",
-  settingsAddCustomNetwork: () => "/settings/networks/add",
-  settingsEditCustomNetwork: () => "/settings/networks/edit",
-  settingsRemoveCustomNetwork: () => "/settings/networks/remove",
-  settingsDappConnections: () => "/settings/dapp-connections",
-  networkWarning: () => "/network-warning",
-  backupDownload: (isFromSettings?: boolean) =>
-    `/backup-download${isFromSettings ? "?settings" : ""}`,
-  error: () => "/error",
+  welcome: route("/index.html"),
+  newWallet: route("/wallets/new"),
+  backupRecovery: route("/recover/backup"),
+  seedRecovery: route("/recover/seed"),
+  seedRecoveryPassword: route("/recover/seed/password"),
+  setupRecovery: route("/recovery"),
+  setupSeedRecovery: route("/recovery/seed"),
+  confirmSeedRecovery: route("/recovery/seed/confirm"),
+  lockScreen: route("/lock-screen"),
+  accountTokens: route("/account/tokens"),
+  accountNfts: route("/account/nfts"),
+  accountActivity: route("/account/activity"),
+  accountNft: route(
+    (contractAddress: string, tokenId: string) =>
+      `/account/nfts/${contractAddress}/${tokenId}`,
+    `/account/nfts/:contractAddress/:tokenId`,
+  ),
+  upgrade: route("/account/upgrade"),
+  accounts: route("/accounts"),
+  newToken: route("/tokens/new"),
+  token: route(
+    (tokenAddress: string) => `/tokens/${tokenAddress}`,
+    "/tokens/:tokenAddress",
+  ),
+  hideToken: route(
+    (tokenAddress: string) => `/tokens/${tokenAddress}/hide`,
+    "/tokens/:tokenAddress/hide",
+  ),
+  reset: route("/reset"),
+  disclaimer: route("/disclaimer"),
+  legacy: route("/legacy"),
+  settings: route("/settings"),
+  settingsNetworks: route("/settings/networks"),
+  settingsAddCustomNetwork: route("/settings/networks/add"),
+  settingsEditCustomNetwork: route("/settings/networks/edit"),
+  settingsRemoveCustomNetwork: route("/settings/networks/remove"),
+  settingsDappConnections: route("/settings/dapp-connections"),
+  networkWarning: route("/network-warning"),
+  backupDownload: route(
+    (isFromSettings?: boolean) =>
+      `/backup-download${isFromSettings ? "?settings" : ""}`,
+    "/backup-download",
+  ),
+  error: route("/error"),
 }
