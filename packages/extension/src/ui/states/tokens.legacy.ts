3bcb4391171b6ed6cb93d5ab75e305255e6ffa34
Janek Rahrt <janek.rahrt@me.com>
2022-05-06 12:49:36 +0200

    feat: add token migration

---
+++ packages/extension/src/ui/states/tokens.legacy.ts
@@ -0,0 +1,102 @@
+// please remove in a future release
+import { BigNumber } from "ethers"
+import create from "zustand"
+import { persist } from "zustand/middleware"
+
+import { equalToken, parsedDefaultTokens } from "../../shared/token"
+import { isValidAddress } from "../utils/addresses"
+import { addToken } from "../utils/messaging"
+import {
+  TokenDetails,
+  mapTokenDetailsToToken,
+  mapTokenToTokenDetails,
+} from "./tokens"
+
+interface State {
+  tokens: TokenDetails[]
+  migrated?: boolean
+}
+
+// restore legacy tokens from localStorage
+export const useTokens = create<State>(
+  persist(
+    () => ({
+      tokens: parsedDefaultTokens.map(mapTokenToTokenDetails),
+    }),
+    {
+      name: "tokens", // name of item in the storage (must be unique)
+      getStorage: () => localStorage, // (optional) by default the 'localStorage' is used
+      merge: (persistedState, currentState) => ({
+        ...currentState,
+        ...persistedState,
+        tokens: [...currentState.tokens, ...persistedState.tokens],
+      }),
+      partialize: (state) =>
+        Object.fromEntries(
+          Object.entries(state).map(([key, value]) => {
+            if (key === "tokens") {
+              return [
+                key,
+                (value as TokenDetails[]).filter(
+                  (token) =>
+                    !parsedDefaultTokens.some((defaultToken) =>
+                      equalToken(token, defaultToken),
+                    ),
+                ),
+              ]
+            }
+            return [key, value]
+          }),
+        ),
+      deserialize: (str) =>
+        JSON.parse(str, (_, v) => {
+          if (
+            typeof v === "object" &&
+            "type" in v &&
+            "hex" in v &&
+            v.type === "BigNumber"
+          ) {
+            return BigNumber.from(v.hex)
+          }
+          return v
+        }),
+    },
+  ),
+)
+
+const isDataComplete = (
+  data: Partial<TokenDetails>,
+): data is Required<TokenDetails> => {
+  if (
+    data.address &&
+    isValidAddress(data.address) &&
+    data.decimals?.toString() &&
+    data.name &&
+    data.symbol
+  ) {
+    return true
+  }
+  return false
+}
+
+let invoked = false
+export async function migrateUiTokensToBackground() {
+  const state = useTokens.getState()
+  if (!state.migrated && !invoked) {
+    invoked = true
+    const customTokens = state.tokens.filter(
+      (token) =>
+        !parsedDefaultTokens.some((defaultToken) =>
+          equalToken(token, defaultToken),
+        ),
+    )
+    const promises = customTokens.map(async (token) => {
+      if (isDataComplete(token)) {
+        await addToken(mapTokenDetailsToToken(token))
+      }
+    })
+
+    await Promise.allSettled(promises)
+  }
+  return useTokens.setState({ migrated: true })
+}
