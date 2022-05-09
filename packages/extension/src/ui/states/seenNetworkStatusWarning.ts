8c55bbba3776e79c70db76fe7067b95f83a32306
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-05-09 13:26:08 +0200

    chore: remove unused assets

--- packages/extension/src/ui/states/seenNetworkStatusWarning.ts
+++ packages/extension/src/ui/states/seenNetworkStatusWarning.ts
@@ -1,7 +1,7 @@
 import create from "zustand"
 import { persist } from "zustand/middleware"
 
-export interface SeenNetworkStatusState {
+interface SeenNetworkStatusState {
   lastSeen: number
   updateLastSeen: () => void
 }
