c5c358c1d52499ce82de80ced990a355495fc03f
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-05-19 11:34:12 +0200

    refactor(inpage): pull out ArgentXAccount

---
+++ packages/extension/src/inpage/ArgentXAccount.ts
@@ -0,0 +1,105 @@
+import {
+  Abi,
+  Account,
+  AddTransactionResponse,
+  Call,
+  InvocationsDetails,
+  Provider,
+  Signature,
+  defaultProvider,
+  ec,
+  typedData,
+} from "starknet"
+
+import { sendMessage, waitForMsgOfType } from "./messageActions"
+
+export class ArgentXAccount extends Account {
+  constructor(address: string, provider?: Provider) {
+    // since account constructor is taking a KeyPair,
+    // we set a dummy one (never used anyway)
+    const keyPair = ec.getKeyPair(0)
+    super(provider || defaultProvider, address, keyPair)
+  }
+
+  public override async execute(
+    transactions: Call | Call[],
+    abis?: Abi[],
+    transactionsDetail?: InvocationsDetails,
+  ): Promise<AddTransactionResponse> {
+    sendMessage({
+      type: "EXECUTE_TRANSACTION",
+      data: { transactions, abis, transactionsDetail },
+    })
+    const { actionHash } = await waitForMsgOfType(
+      "EXECUTE_TRANSACTION_RES",
+      1000,
+    )
+    sendMessage({ type: "OPEN_UI" })
+
+    const result = await Promise.race([
+      waitForMsgOfType(
+        "TRANSACTION_SUBMITTED",
+        11 * 60 * 1000,
+        (x) => x.data.actionHash === actionHash,
+      ),
+      waitForMsgOfType(
+        "TRANSACTION_FAILED",
+        10 * 60 * 1000,
+        (x) => x.data.actionHash === actionHash,
+      )
+        .then(() => "error" as const)
+        .catch(() => {
+          sendMessage({ type: "TRANSACTION_FAILED", data: { actionHash } })
+          return "timeout" as const
+        }),
+    ])
+
+    if (result === "error") {
+      throw Error("User abort")
+    }
+    if (result === "timeout") {
+      throw Error("User action timed out")
+    }
+
+    return {
+      code: "TRANSACTION_RECEIVED",
+      address: this.address,
+      transaction_hash: result.txHash,
+    }
+  }
+
+  public override async signMessage(
+    data: typedData.TypedData,
+  ): Promise<Signature> {
+    sendMessage({ type: "SIGN_MESSAGE", data })
+    const { actionHash } = await waitForMsgOfType("SIGN_MESSAGE_RES", 1000)
+    sendMessage({ type: "OPEN_UI" })
+
+    const result = await Promise.race([
+      waitForMsgOfType(
+        "SIGNATURE_SUCCESS",
+        11 * 60 * 1000,
+        (x) => x.data.actionHash === actionHash,
+      ),
+      waitForMsgOfType(
+        "SIGNATURE_FAILURE",
+        10 * 60 * 1000,
+        (x) => x.data.actionHash === actionHash,
+      )
+        .then(() => "error" as const)
+        .catch(() => {
+          sendMessage({ type: "SIGNATURE_FAILURE", data: { actionHash } })
+          return "timeout" as const
+        }),
+    ])
+
+    if (result === "error") {
+      throw Error("User abort")
+    }
+    if (result === "timeout") {
+      throw Error("User action timed out")
+    }
+
+    return [result.r, result.s]
+  }
+}
