7a2903cd1ee10f4eb28e154bed9ced20c04245e9
Simon Heys <si@simonheys.com>
2022-06-14 11:34:46 +0100

    feat: show send transaction, account and network details

--- packages/extension/src/ui/features/actions/ApproveTransactionScreen.tsx
+++ packages/extension/src/ui/features/actions/ApproveTransactionScreen.tsx
@@ -1,9 +1,24 @@
+import { isArray } from "lodash-es"
 import { FC, useState } from "react"
 import { Navigate } from "react-router-dom"
 import { Call } from "starknet"
 import styled from "styled-components"
 
+import {
+  Field,
+  FieldGroup,
+  FieldKey,
+  FieldValue,
+} from "../../components/Fields"
+import { formatTokenBalance } from "../../features/accountTokens/tokens.service"
 import { routes } from "../../routes"
+import { formatTruncatedAddress } from "../../services/addresses"
+import {
+  getAccountName,
+  useAccountMetadata,
+} from "../accounts/accountMetadata.state"
+import { getAccountImageUrl } from "../accounts/accounts.service"
+import { ProfilePicture } from "../accounts/ProfilePicture"
 import { ConfirmPageProps, ConfirmScreen } from "./ConfirmScreen"
 import { FeeEstimation } from "./FeeEstimation"
 
@@ -23,6 +38,73 @@ const Pre = styled.pre`
   background: #161616;
 `
 
+const AccountName = styled.div`
+  margin-left: 8px;
+`
+
+export interface TransactionDetailProps {
+  transaction: Call
+}
+
+/** Renders a single transaction */
+
+export const TransactionDetail: FC<TransactionDetailProps> = ({
+  transaction,
+}) => {
+  const displayAddress = formatTruncatedAddress(transaction.contractAddress)
+  if (transaction.entrypoint === "transfer") {
+    /** amount is second entry in calldata array */
+    const displayAmount = transaction.calldata
+      ? formatTokenBalance(transaction.calldata[1])
+      : "–"
+    return (
+      <FieldGroup>
+        <Field>
+          <FieldKey>Send</FieldKey>
+          <FieldValue>{displayAmount}</FieldValue>
+        </Field>
+        <Field>
+          <FieldKey>To</FieldKey>
+          <FieldValue>{displayAddress}</FieldValue>
+        </Field>
+      </FieldGroup>
+    )
+  }
+  return (
+    <FieldGroup>
+      <Field>
+        <FieldKey>Contract</FieldKey>
+        <FieldValue>{displayAddress}</FieldValue>
+      </Field>
+      <Field>
+        <FieldKey>Action</FieldKey>
+        <FieldValue>{transaction.entrypoint}</FieldValue>
+      </Field>
+    </FieldGroup>
+  )
+}
+
+export interface TransactionDetailsProps {
+  transactions: Call | Call[]
+}
+
+/** Renders one or more transactions */
+
+export const TransactionDetails: FC<TransactionDetailsProps> = ({
+  transactions,
+}) => {
+  const transactionsAsArray: Call[] = isArray(transactions)
+    ? transactions
+    : [transactions]
+  return (
+    <>
+      {transactionsAsArray.map((transaction, index) => (
+        <TransactionDetail key={index} transaction={transaction} />
+      ))}
+    </>
+  )
+}
+
 export const ApproveTransactionScreen: FC<ApproveTransactionScreenProps> = ({
   transactions,
   selectedAccount,
@@ -31,10 +113,13 @@ export const ApproveTransactionScreen: FC<ApproveTransactionScreenProps> = ({
   ...props
 }) => {
   const [disableConfirm, setDisableConfirm] = useState(true)
+  const { accountNames } = useAccountMetadata()
 
   if (!selectedAccount) {
     return <Navigate to={routes.accounts()} />
   }
+
+  const accountName = getAccountName(selectedAccount, accountNames)
 
   return (
     <ConfirmScreen
@@ -56,6 +141,24 @@ export const ApproveTransactionScreen: FC<ApproveTransactionScreenProps> = ({
       }
       {...props}
     >
+      <TransactionDetails transactions={transactions} />
+      <FieldGroup>
+        <Field>
+          <FieldKey>From</FieldKey>
+          <FieldValue>
+            <ProfilePicture
+              src={getAccountImageUrl(accountName, selectedAccount.address)}
+              small
+              disabled
+            />
+            <AccountName>{accountName}</AccountName>
+          </FieldValue>
+        </Field>
+        <Field>
+          <FieldKey>Network</FieldKey>
+          <FieldValue>{selectedAccount.network.name}</FieldValue>
+        </Field>
+      </FieldGroup>
       <Pre>{JSON.stringify(transactions, null, 2)}</Pre>
     </ConfirmScreen>
   )
