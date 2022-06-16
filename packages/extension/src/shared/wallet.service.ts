9a11cc495f6e53699a2382357ddf7de5d1cb2bb0
Janek Rahrt <janek.rahrt@me.com>
2022-06-16 16:14:40 +0200

    fix: remove console log

--- packages/extension/src/shared/wallet.service.ts
+++ packages/extension/src/shared/wallet.service.ts
@@ -9,7 +9,6 @@ export const hasNewDerivationPath = (derivationPath?: string): boolean =>
   Boolean(derivationPath?.startsWith(baseDerivationPath))
 
 export const isDeprecated = ({ signer, network }: WalletAccount): boolean => {
-  console.log("isDeprecated", signer.derivationPath)
   return (
     Boolean(network.accountClassHash) &&
     !hasNewDerivationPath(signer.derivationPath)
