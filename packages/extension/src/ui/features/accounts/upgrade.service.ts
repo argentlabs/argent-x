ca3b9cb951d13632973d903c8a124434919c97e5
Janek Rahrt <janek.rahrt@me.com>
2022-06-16 10:35:48 +0200

    fix: upgrade view

--- packages/extension/src/ui/features/accounts/upgrade.service.ts
+++ packages/extension/src/ui/features/accounts/upgrade.service.ts
@@ -1,12 +1,16 @@
 import { number } from "starknet"
 
+import { hasNewDerivationPath } from "../../../shared/wallet.service"
 import { Account } from "./Account"
 
 export async function checkIfUpgradeAvailable(
   account: Account,
   targetImplementation?: string,
 ): Promise<boolean> {
-  if (!targetImplementation) {
+  if (
+    !targetImplementation ||
+    !hasNewDerivationPath(account.signer.derivationPath) // old accounts are not upgradable anymore
+  ) {
     return false
   }
   const currentImplementation = await account.getCurrentImplementation()
