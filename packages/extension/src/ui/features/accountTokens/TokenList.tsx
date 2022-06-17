7d932503bd15b2c2329c49325b23b66b1b3b5d0e
Simon Heys <si@simonheys.com>
2022-06-17 15:22:47 +0100

    feat: account balance styling

--- packages/extension/src/ui/features/accountTokens/TokenList.tsx
+++ packages/extension/src/ui/features/accountTokens/TokenList.tsx
@@ -1,12 +1,9 @@
 import { FC } from "react"
 import { Link } from "react-router-dom"
-import styled from "styled-components"
 
-import { prettifyCurrencyValue } from "../../../shared/tokenPrice.service"
 import { routes } from "../../routes"
 import { SectionHeader } from "../accounts/SectionHeader"
 import { TokenListItem } from "./TokenListItem"
-import { useSumTokenBalancesToCurrencyValue } from "./tokenPriceHooks"
 import { useTokensWithBalance } from "./tokens.state"
 
 interface TokenListProps {
@@ -15,21 +12,11 @@ interface TokenListProps {
   canShowEmptyAccountAlert?: boolean
 }
 
-const TotalCurrencyValue = styled(SectionHeader)`
-  text-align: center;
-`
-
 export const TokenList: FC<TokenListProps> = ({ showTitle }) => {
   const { isValidating, tokenDetails } = useTokensWithBalance()
-  const sumCurrencyValue = useSumTokenBalancesToCurrencyValue(tokenDetails)
   return (
     <>
       {showTitle && <SectionHeader>Tokens</SectionHeader>}
-      {sumCurrencyValue !== undefined && (
-        <TotalCurrencyValue>
-          Total {prettifyCurrencyValue(sumCurrencyValue)}
-        </TotalCurrencyValue>
-      )}
       {tokenDetails.map((token) => (
         <Link key={token.address} to={routes.token(token.address)}>
           <TokenListItem token={token} isLoading={isValidating} />
