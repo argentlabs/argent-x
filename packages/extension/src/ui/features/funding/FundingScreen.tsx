d72c973720dfcb9a260d9f1febba1dd7bfc0da26
Janek Rahrt <janek.rahrt@me.com>
2022-06-21 10:58:41 +0200

    feat(onramp): add banxa

--- packages/extension/src/ui/features/funding/FundingScreen.tsx
+++ packages/extension/src/ui/features/funding/FundingScreen.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react"
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
 import styled from "styled-components"
 
-import { useAppState } from "../../app.state"
 import { IconBar } from "../../components/IconBar"
 import { Option, OptionsWrapper } from "../../components/Options"
 import { PageWrapper } from "../../components/Page"
 import { routes } from "../../routes"
+import { normalizeAddress } from "../../services/addresses"
+import { useSelectedAccount } from "../accounts/accounts.state"
 import CardSvg from "./card.svg"
 import EthereumSvg from "./ethereum.svg"
 import StarkNetSvg from "./starknet.svg"
@@ -21,12 +22,20 @@ const Title = styled.h1`
 `
 
 export const FundingScreen: FC = () => {
-  const { switcherNetworkId } = useAppState()
-  const bridgeUrl =
-    switcherNetworkId === "mainnet-alpha"
-      ? "https://starkgate.starknet.io"
-      : switcherNetworkId === "goerli-alpha" &&
-        "https://goerli.starkgate.starknet.io"
+  const account = useSelectedAccount()
+
+  if (!account) {
+    return <Navigate to={routes.accounts()} />
+  }
+
+  const isMainnet = account.networkId === "mainnet-alpha"
+  const bridgeUrl = isMainnet
+    ? "https://starkgate.starknet.io"
+    : account.networkId === "goerli-alpha" &&
+      "https://goerli.starkgate.starknet.io"
+
+  const isDeprecatedAccount = false // isDeprecated(account) // Allow purchases on deprecated accounts as some people may want to buy some eth to transfer funds out of their wallet
+  const allowFiatPurchase = isMainnet && !isDeprecatedAccount
 
   return (
     <>
@@ -34,13 +43,32 @@ export const FundingScreen: FC = () => {
       <PageWrapper>
         <Title>How would you like to fund your account?</Title>
         <OptionsWrapper>
-          <Option
-            title="Buy with card or bank transfer"
-            description="Coming soon"
-            icon={<CardSvg />}
-            disabled
-            hideArrow
-          />
+          {allowFiatPurchase ? (
+            <a
+              href={`http://argentx.banxa.com/?walletAddress=${normalizeAddress(
+                account.address,
+              )}`}
+              target="_blank"
+            >
+              <Option
+                title="Buy with card or bank transfer"
+                description={"Purchase using fiat via Banxa"}
+                icon={<CardSvg />}
+              />
+            </a>
+          ) : (
+            <Option
+              title="Buy with card or bank transfer"
+              description={
+                !isMainnet
+                  ? "Only available on Mainnet"
+                  : "Only available for new accounts"
+              }
+              icon={<CardSvg />}
+              disabled
+              hideArrow
+            />
+          )}
           <Link to={routes.fundingQrCode()}>
             <Option
               title="From another StarkNet account"
