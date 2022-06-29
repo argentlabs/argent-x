7a117f44df84883c445ad2c4cebbad1a58195786
Janek Rahrt <janek.rahrt@me.com>
2022-06-29 15:47:54 +0200

    test: e2e

---
+++ packages/extension/e2e/fixture.ts
@@ -0,0 +1,23 @@
+import path from "path"
+
+import { test as base, chromium, firefox, webkit } from "@playwright/test"
+
+export const test = base.extend({
+  context: async ({ browserName }, use, { workerIndex }) => {
+    console.log("[INFO] Extending browser context")
+    const browserTypes = { chromium, firefox, webkit }
+    const extensionPath = path.join(__dirname, "../dist/")
+    const context = await browserTypes[browserName].launchPersistentContext(
+      `/tmp/e2e-argent-x-${workerIndex}`,
+      {
+        headless: false,
+        args: [
+          `--disable-extensions-except=${extensionPath}`,
+          `--load-extension=${extensionPath}`,
+        ],
+      },
+    )
+    await use(context)
+    await context.close()
+  },
+})
