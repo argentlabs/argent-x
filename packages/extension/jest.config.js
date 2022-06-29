98e4070cd00d27f7a38ebf42309ecc91ec48474b
Janek Rahrt <janek.rahrt@me.com>
2022-06-29 16:11:52 +0200

    fix: jest

--- packages/extension/jest.config.js
+++ packages/extension/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     "^.+\\.(ts|tsx|js)?$": "ts-jest",
   },
   transformIgnorePatterns: ["node_modules/(?!(url-join|lodash-es)/)"],
+  testPathIgnorePatterns: ["/node_modules/", "/e2e/"],
   setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
 }
