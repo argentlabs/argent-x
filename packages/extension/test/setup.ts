02136c77df9dcca8847ea62733334761283c5631
Janek Rahrt <janek.rahrt@me.com>
2022-07-05 19:46:54 +0200

    fix: tests

--- packages/extension/test/setup.ts
+++ packages/extension/test/setup.ts
@@ -1,5 +1,4 @@
 import fetch from "cross-fetch"
-import { chrome } from "jest-chrome"
 
 try {
   global.fetch = fetch
@@ -12,10 +11,11 @@ try {
   // do nothing
 }
 try {
-  ;(window as any).chrome = chrome
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  ;(window as any).chrome.runtime.id = "test"
+  ;(window as any).chrome = {
+    runtime: {
+      id: "test",
+    },
+  }
 } catch {
   // do nothing
 }
