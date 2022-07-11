fd85b08b449827d556f67a13ba01aa95b508f605
Dhruv Kelawala <dhruvrk2000@gmail.com>
2022-07-11 16:24:00 +0100

    fix: add ThemeProviders for Storybook

--- packages/storybook/.storybook/decorators.tsx
+++ packages/storybook/.storybook/decorators.tsx
@@ -1,16 +1,24 @@
-import { GlobalStyle, theme } from "@argent-x/extension/src/ui/theme"
-import { ThemeProvider } from "@mui/material"
+import {
+  FixedGlobalStyle,
+  ThemeProvider as StyledComponentsThemeProvider,
+  ThemedGlobalStyle,
+  muiTheme,
+} from "@argent-x/extension/src/ui/theme"
+import { ThemeProvider as MuiThemeProvider } from "@mui/material"
 import React from "react"
 
 export const decorators = [
   (Story) => (
-    <ThemeProvider theme={theme}>
+    <MuiThemeProvider theme={muiTheme}>
       <link
         href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;600;700;900&display=swap"
         rel="stylesheet"
       />
-      <GlobalStyle extensionIsInTab={false} />
-      <Story />
-    </ThemeProvider>
+      <FixedGlobalStyle extensionIsInTab={false} />
+      <StyledComponentsThemeProvider>
+        <ThemedGlobalStyle />
+        <Story />
+      </StyledComponentsThemeProvider>
+    </MuiThemeProvider>
   ),
 ]
