19fc4e900d530e89026353490c8a2808259d2d0d
Dhruv Kelawala <dhruvrk2000@gmail.com>
2022-07-22 14:02:03 +0100

    fix: remove old setting storage

--- packages/extension/src/background/storage/settings.ts
+++
@@ -1,7 +0,0 @@
-import { ISettingsStorage, defaultSettings } from "../../shared/settings"
-import { Storage } from "."
-
-export const settingsStorage = new Storage<ISettingsStorage>(
-  defaultSettings,
-  "settings",
-)
