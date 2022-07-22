ede57473c16916d3a71c2118fbe1d19483942a84
Dhruv Kelawala <dhruvrk2000@gmail.com>
2022-07-22 14:05:50 +0100

    feat: add better typings to setting handlers

--- packages/extension/src/shared/settings/index.ts
+++ packages/extension/src/shared/settings/index.ts
@@ -6,17 +6,20 @@ export const getAllSettings = async () => {
   return await settingsStorage.get()
 }
 
-export const getSetting = async (
-  key: SettingsStorageKey,
+export const getSetting = async <K extends SettingsStorageKey>(
+  key: K,
 ): Promise<SettingsStorageValue> => {
   const allSettings = await getAllSettings()
 
   return allSettings[key]
 }
 
-export const setSetting = async (
-  key: SettingsStorageKey,
-  value: SettingsStorageValue,
+export const setSetting = async <
+  K extends SettingsStorageKey,
+  V extends SettingsStorageValue,
+>(
+  key: K,
+  value: V,
 ) => {
   return await settingsStorage.set({ [key]: value })
 }
