37a2ac1b6aede4aef305c74f65a8d5453bee3b1f
Simon Heys <si@simonheys.com>
2022-09-26 17:24:17 +0100

    fix: conditional ledger UI in e2e

--- packages/extension/e2e/steps/newAccount.ts
+++ packages/extension/e2e/steps/newAccount.ts
@@ -1,8 +1,12 @@
 import { expect } from "@playwright/test"
 import type { Page } from "@playwright/test"
 
+const isLedgerEnabled = (process.env.FEATURE_LEDGER || "false") === "true"
+
 export async function newAccount(page: Page) {
   await expect(page.locator("h1:has-text('Accounts')")).toBeVisible()
   await page.click("[aria-label='Create new wallet']")
-  await page.click("text=Create new ArgentX account")
+  if (isLedgerEnabled) {
+    await page.click("text=Create new ArgentX account")
+  }
 }
