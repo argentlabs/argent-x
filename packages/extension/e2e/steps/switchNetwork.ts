d4d79bbe4834f43d55ea078ee18db967751015b2
Dhruv Kelawala <dhruvrk2000@gmail.com>
2022-11-02 19:22:11 +0530

    fix: e2e

--- packages/extension/e2e/steps/switchNetwork.ts
+++ packages/extension/e2e/steps/switchNetwork.ts
@@ -2,16 +2,12 @@ import { expect } from "@playwright/test"
 import type { Page } from "@playwright/test"
 
 export async function switchNetwork(page: Page, targetNetworkName: string) {
-  const networkSwitcher = await page.$(
-    "[aria-label='Selected network'] >> xpath=..",
-  )
+  const networkSwitcher = await page
+    .locator("[aria-label='Selected network']")
+    .elementHandle()
   if (!networkSwitcher) {
     return expect(networkSwitcher).toBeTruthy()
   }
   await networkSwitcher.hover()
-  const networkToSelect = await networkSwitcher.$(`text=${targetNetworkName}`)
-  if (!networkToSelect) {
-    return expect(networkToSelect).toBeTruthy()
-  }
-  await networkToSelect.click()
+  await page.locator(`text=${targetNetworkName}`).click()
 }
