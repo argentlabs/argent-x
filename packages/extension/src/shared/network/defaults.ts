a47745889561d15dedc90c8dc3f513b36d1f8c68
Janek <janek.rahrt@me.com>
2022-11-04 20:33:52 +0100

    feat: release with new accounts for mainnet

--- packages/extension/src/shared/network/defaults.ts
+++ packages/extension/src/shared/network/defaults.ts
@@ -13,21 +13,6 @@ const DEV_ONLY_NETWORKS: Network[] = [
     multicallAddress:
       "0x02acfa68f063b35ffd9e6a6b77ba0d7ad231d1a64f9fa8c5770e0be376e17ef9",
   },
-  {
-    id: "goerli-alpha-2",
-    name: "Testnet 2",
-    chainId: "SN_GOERLI",
-    baseUrl: "https://alpha4-2.starknet.io",
-    explorerUrl: "https://goerli-2.voyager.online/",
-    accountClassHash: {
-      argentAccount:
-        "0x1a7820094feaf82d53f53f214b81292d717e7bb9a92bb2488092cd306f3993f",
-      argentPluginAccount:
-        "0xebe4b44d154bc07eacad202ee19757cdc73e7d4c672bc20d9031450c6db3ad",
-    },
-    multicallAddress:
-      "0x0648aa7b71687449382180c76fd2871e1e77ccb6775e8d29b79e3d8c8b512380",
-  },
 ]
 
 export const defaultNetworks: Network[] = [
@@ -39,7 +24,9 @@ export const defaultNetworks: Network[] = [
     explorerUrl: "https://voyager.online",
     accountClassHash: {
       argentAccount:
-        "0x3e327de1c40540b98d05cbcb13552008e36f0ec8d61d46956d2f9752c294328",
+        "0x1a7820094feaf82d53f53f214b81292d717e7bb9a92bb2488092cd306f3993f",
+      argentPluginAccount:
+        "0xebe4b44d154bc07eacad202ee19757cdc73e7d4c672bc20d9031450c6db3ad",
     },
     multicallAddress:
       "0x0740a7a14618bb7e4688d10059bc42104d22c315bb647130630c77d3b6d3ee50",
@@ -60,6 +47,21 @@ export const defaultNetworks: Network[] = [
     multicallAddress:
       "0x042a12c5a641619a6c58e623d5735273cdfb0e13df72c4bacb4e188892034bd6",
     readonly: true,
+  },
+  {
+    id: "goerli-alpha-2",
+    name: "Testnet 2",
+    chainId: "SN_GOERLI",
+    baseUrl: "https://alpha4-2.starknet.io",
+    explorerUrl: "https://goerli-2.voyager.online/",
+    accountClassHash: {
+      argentAccount:
+        "0x1a7820094feaf82d53f53f214b81292d717e7bb9a92bb2488092cd306f3993f",
+      argentPluginAccount:
+        "0xebe4b44d154bc07eacad202ee19757cdc73e7d4c672bc20d9031450c6db3ad",
+    },
+    multicallAddress:
+      "0x0648aa7b71687449382180c76fd2871e1e77ccb6775e8d29b79e3d8c8b512380",
   },
   ...(process.env.NODE_ENV === "development" ? DEV_ONLY_NETWORKS : []),
   {
