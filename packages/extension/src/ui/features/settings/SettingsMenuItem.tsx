c61cbdfaccaf5cbb5b1cd160196fdac7e93613b7
bluecco <bluecco@gmail.com>
2022-11-10 17:57:50 +0100

    feat: update settings screen ui

--- packages/extension/src/ui/features/settings/SettingsMenuItem.tsx
+++ packages/extension/src/ui/features/settings/SettingsMenuItem.tsx
@@ -1,34 +1,41 @@
 import { H6, icons } from "@argent/ui"
-import { Box } from "@chakra-ui/react"
-import { FC } from "react"
+import { Flex, Text } from "@chakra-ui/react"
+import { FC, ReactNode } from "react"
 import { Link } from "react-router-dom"
 
 const { ChevronRightIcon } = icons
 
 interface SettingsMenuItemProps {
+  leftIcon?: ReactNode
+  rightIcon?: ReactNode
   onClick?: () => void
   title: string
   to: string
 }
 
 const SettingsMenuItem: FC<SettingsMenuItemProps> = ({
+  leftIcon,
+  rightIcon,
   onClick,
   to,
   title,
 }) => (
   <Link to={to} onClick={onClick}>
-    <Box
+    <Flex
       borderRadius="8"
       backgroundColor="neutrals.800"
       display="flex"
       justifyContent="space-between"
       alignItems="center"
-      py="6"
-      px="8"
+      px="6"
+      py="5"
     >
-      <H6>{title}</H6>
-      <ChevronRightIcon fontSize="inherit" />
-    </Box>
+      <Flex gap="4">
+        {leftIcon && <Text fontSize="xl">{leftIcon}</Text>}
+        <H6>{title}</H6>
+      </Flex>
+      {rightIcon ? <>{rightIcon}</> : <ChevronRightIcon fontSize="inherit" />}
+    </Flex>
   </Link>
 )
 
--- packages/extension/src/ui/features/settings/SettingsScreen.tsx
+++ packages/extension/src/ui/features/settings/SettingsScreen.tsx
@@ -1,8 +1,9 @@
-import { BarBackButton, NavigationBar } from "@argent/ui"
-import { Stack } from "@chakra-ui/react"
+import { B2, BarBackButton, NavigationContainer, icons } from "@argent/ui"
+import { Box, Flex, Stack, Text } from "@chakra-ui/react"
 import { FC } from "react"
 import { useNavigate } from "react-router-dom"
 import { useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
 import styled from "styled-components"
 
 import { isPrivacySettingsEnabled } from "../../../shared/settings"
@@ -10,9 +11,21 @@ import { Button } from "../../components/Button"
 import { routes } from "../../routes"
 import { stopSession } from "../../services/backgroundSessions"
 import { H2 } from "../../theme/Typography"
+import { AccountListScreenItem } from "../accounts/AccountListScreenItem"
+import { useAccount, useSelectedAccountStore } from "../accounts/accounts.state"
 import { useExtensionIsInTab, useOpenExtensionInTab } from "../browser/tabs"
 import { SettingsMenuItem } from "./SettingsMenuItem"
-import { SupportFooter } from "./SupportFooter"
+
+const {
+  LockIcon,
+  AddressBookIcon,
+  CodeIcon,
+  ExpandIcon,
+  ExtendedIcon,
+  LinkIcon,
+  PasswordIcon,
+  ShieldIcon,
+} = icons
 
 export const Title = styled.h3`
   font-weight: 600;
@@ -66,54 +79,82 @@ export const SettingsScreen: FC = () => {
   const extensionIsInTab = useExtensionIsInTab()
   const { pathname: returnTo } = useLocation()
   const navigate = useNavigate()
+  const { selectedAccount } = useSelectedAccountStore()
+  const account = useAccount(selectedAccount)
   return (
     <>
-      <NavigationBar
+      <NavigationContainer
         leftButton={<BarBackButton onClick={() => navigate(-1)} />}
         title={"Settings"}
-      />
-
-      <Stack direction="column" mx="4">
-        <SettingsMenuItem
+      >
+        <Box mx="4" my="4">
+          {account && <AccountListScreenItem account={account} />}
+        </Box>
+        <Stack direction="column" mx="4" h="60vh">
+          {/*   <SettingsMenuItem
           onClick={stopSession}
           to={routes.lockScreen()}
           title="Lock wallet"
-        />
+        /> */}
 
-        {!extensionIsInTab && (
+          {!extensionIsInTab && (
+            <SettingsMenuItem
+              leftIcon={<ExtendedIcon />}
+              rightIcon={<ExpandIcon />}
+              to={routes.settings()}
+              onClick={openExtensionInTab}
+              title="Extended view"
+            />
+          )}
+
           <SettingsMenuItem
-            to={routes.settings()}
-            onClick={openExtensionInTab}
-            title="Extended view"
+            leftIcon={<AddressBookIcon />}
+            to={routes.settingsAddressbook()}
+            title="Address book"
           />
-        )}
 
-        <SettingsMenuItem
-          to={routes.settingsAddressbook()}
-          title="Address book"
-        />
+          <SettingsMenuItem
+            leftIcon={<LinkIcon />}
+            to={routes.settingsDappConnections()}
+            title="Connected dapps"
+          />
 
-        <SettingsMenuItem
-          to={routes.settingsDappConnections()}
-          title="Reset dapp connections"
-        />
+          <SettingsMenuItem
+            leftIcon={<PasswordIcon />}
+            to={routes.settingsSeed(returnTo)}
+            title="Show recovery phrase"
+          />
 
-        <SettingsMenuItem
-          to={routes.settingsSeed(returnTo)}
-          title="Show recovery phrase"
-        />
+          {isPrivacySettingsEnabled && (
+            <SettingsMenuItem
+              leftIcon={<ShieldIcon />}
+              to={routes.settingsPrivacy()}
+              title="Privacy"
+            />
+          )}
 
-        {isPrivacySettingsEnabled && (
-          <SettingsMenuItem to={routes.settingsPrivacy()} title="Privacy" />
-        )}
-
-        <SettingsMenuItem
-          to={routes.settingsDeveloper()}
-          title="Developer settings"
-        />
-
-        <SupportFooter />
-      </Stack>
+          <SettingsMenuItem
+            leftIcon={<CodeIcon />}
+            to={routes.settingsDeveloper()}
+            title="Developer settings"
+          />
+        </Stack>
+      </NavigationContainer>
+      <Link onClick={stopSession} to={routes.lockScreen()}>
+        <Flex
+          py={6}
+          gap={2}
+          cursor="pointer"
+          justifyContent="center"
+          alignItems="center"
+          color="white50"
+        >
+          <Text fontSize="base">
+            <LockIcon />
+          </Text>
+          <B2>Lock wallet</B2>
+        </Flex>
+      </Link>
     </>
   )
 }
