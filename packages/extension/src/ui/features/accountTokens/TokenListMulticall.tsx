bd10e9a2644d232fda2be2d26f0d57677b5440a9
Simon Heys <si@simonheys.com>
2022-11-17 11:42:03 +0000

    chore: reinstate error boundary

--- packages/extension/src/ui/features/accountTokens/TokenListMulticall.tsx
+++ packages/extension/src/ui/features/accountTokens/TokenListMulticall.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@argent/ui"
 import { FC, Suspense } from "react"
 import { useNavigate } from "react-router-dom"
 
 import { Token } from "../../../shared/token/type"
 import { useAppState } from "../../app.state"
+import { ErrorBoundary } from "../../components/ErrorBoundary"
+import ErrorBoundaryFallbackWithCopyError from "../../components/ErrorBoundaryFallbackWithCopyError"
 import { routes } from "../../routes"
 import { useSelectedAccount } from "../accounts/accounts.state"
 import { NewTokenButton } from "./NewTokenButton"
@@ -34,24 +37,32 @@ export const TokenListMulticall: FC<TokenListMulticallProps> = ({
   }
   const tokens = tokenList || tokensInNetwork
   return (
-    <Suspense fallback={<NewTokenButton isLoading />}>
-      {tokens.map((token) => (
-        <TokenListItemMulticall
-          key={token.address}
-          account={account}
-          token={token}
-          variant={variant}
-          showTokenSymbol={showTokenSymbol}
-          onClick={() => {
-            navigate(
-              navigateToSend
-                ? routes.sendToken(token.address)
-                : routes.token(token.address),
-            )
-          }}
+    <ErrorBoundary
+      fallback={
+        <ErrorBoundaryFallbackWithCopyError
+          message={"Sorry, an error occurred fetching tokens"}
         />
-      ))}
-      {showNewTokenButton && <NewTokenButton />}
-    </Suspense>
+      }
+    >
+      <Suspense fallback={<NewTokenButton isLoading />}>
+        {tokens.map((token) => (
+          <TokenListItemMulticall
+            key={token.address}
+            account={account}
+            token={token}
+            variant={variant}
+            showTokenSymbol={showTokenSymbol}
+            onClick={() => {
+              navigate(
+                navigateToSend
+                  ? routes.sendToken(token.address)
+                  : routes.token(token.address),
+              )
+            }}
+          />
+        ))}
+        {showNewTokenButton && <NewTokenButton />}
+      </Suspense>
+    </ErrorBoundary>
   )
 }
