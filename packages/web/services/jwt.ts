a4bff8e2f3d809e427ed50b0e0b9c237ee52d7a8
Janek <janek.rahrt@me.com>
2022-11-21 13:35:24 +0100

    fix: signing key pair

--- packages/web/services/jwt.ts
+++ packages/web/services/jwt.ts
@@ -1,4 +1,9 @@
-import { generateKeyPair } from "jose"
+import {
+  SignJWT,
+  calculateJwkThumbprint,
+  exportJWK,
+  generateKeyPair,
+} from "jose"
 
 import { Device, idb } from "./idb"
 
@@ -7,10 +12,7 @@ let device: Device
 export const genKeyPairOpts = Object.freeze({ extractable: false }) // important that it stays non-extractable
 
 const createDevice = async (): Promise<Device> => {
-  const { privateKey: signingKey } = await generateKeyPair(
-    "ES256",
-    genKeyPairOpts,
-  )
+  const signingKey = await generateKeyPair("ES256", genKeyPairOpts)
 
   const encryptionKey = await generateKeyPair("ECDH-ES+A256KW", genKeyPairOpts)
 
@@ -21,11 +23,30 @@ const createDevice = async (): Promise<Device> => {
   }
 }
 
+createDevice().then(async (d) => {
+  if (typeof d.signingKey === "string") {
+    throw new Error("Signing key is not a key object")
+  }
+  const jwk = await exportJWK(d.signingKey.publicKey)
+  const thumbprint = await calculateJwkThumbprint(jwk)
+  const jwt = await new SignJWT({ foo: "bar" })
+    .setIssuer("https://example.com")
+    .setAudience("https://example.com")
+    .setProtectedHeader({
+      alg: "ES256",
+      jwk: { ...jwk },
+      kid: thumbprint,
+    })
+    .sign(d.signingKey.privateKey)
+
+  console.log(jwt)
+})
+
 export const getDevice = async () => {
   if (!device) {
     const newDevice = await createDevice()
 
-    device = await idb.transaction("r", idb.devices, async () => {
+    device = await idb.transaction("rw", idb.devices, async () => {
       const device = await idb.devices.get(0)
       if (device) {
         return device
