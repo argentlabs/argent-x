30379115b6f863056b190d8e44869f4c5be68be5
bluecco <bluecco@gmail.com>
2022-11-21 15:10:29 +0100

    chore: update app routes and last settings components

--- packages/extension/src/ui/features/settings/NetworkSettingsScreen.tsx
+++ packages/extension/src/ui/features/settings/NetworkSettingsScreen.tsx
@@ -1,3 +1,4 @@
+import { BarBackButton, CellStack, NavigationContainer } from "@argent/ui"
 import { isEqual } from "lodash-es"
 import { FC, useMemo } from "react"
 import { Link, useNavigate } from "react-router-dom"
@@ -8,51 +9,18 @@ import {
   restoreDefaultCustomNetworks,
 } from "../../../shared/network"
 import { defaultCustomNetworks } from "../../../shared/network/storage"
-import { IconBar } from "../../components/IconBar"
 import { IconButton } from "../../components/IconButton"
 import { AddIcon, RefreshIcon } from "../../components/Icons/MuiIcons"
 import { ResponsiveFixedBox } from "../../components/Responsive"
 import { Spinner } from "../../components/Spinner"
 import { routes } from "../../routes"
-import { H2, P } from "../../theme/Typography"
+import { P } from "../../theme/Typography"
 import { useCustomNetworks, useNetworks } from "../networks/useNetworks"
 import { DappConnection } from "./DappConnection"
 import { useSelectedNetwork } from "./selectedNetwork.state"
 
-interface IWrapper {
-  isDefaultCustomNetworks: boolean
-}
-
-const Wrapper = styled.div<IWrapper>`
-  display: flex;
-  flex-direction: column;
-  padding: 0 32px
-    ${({ isDefaultCustomNetworks }) =>
-      isDefaultCustomNetworks ? "24px" : "56px"}
-    32px;
-  ${H2} {
-    margin: 0;
-  }
-`
-
 const IconButtonCenter = styled(IconButton)`
   margin: 0 auto;
-`
-
-const List = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 32px 0 8px;
-  width: 100%;
-
-  > * + * {
-    margin-top: 8px;
-  }
-
-  > * {
-    width: 100%;
-  }
 `
 
 const Footer = styled(ResponsiveFixedBox)`
@@ -107,52 +75,51 @@ export const NetworkSettingsScreen: FC = () => {
   }, [customNetworks])
 
   return (
-    <>
-      <IconBar back />
-      <Wrapper isDefaultCustomNetworks={isDefaultCustomNetworks}>
-        <H2>Networks</H2>
-        <List>
-          {!allNetworks ? (
-            <Spinner />
-          ) : allNetworks.length === 0 ? (
-            <P>No custom networks found</P>
-          ) : (
-            allNetworks.map((network) => (
-              <DappConnection
-                key={network.id}
-                host={network.name}
-                onClick={() => {
-                  setSelectedCustomNetwork(network)
-                  navigate(routes.settingsEditCustomNetwork())
-                }}
-                hideRemove={network.readonly}
-                onRemoveClick={async () => {
-                  await removeNetwork(network.id)
-                }}
-              />
-            ))
-          )}
-        </List>
-
-        <Link to={routes.settingsAddCustomNetwork()}>
-          <IconButtonCenter size={48} style={{ marginTop: "32px" }}>
-            <AddIcon fontSize="large" />
-          </IconButtonCenter>
-        </Link>
-
-        {!isDefaultCustomNetworks && (
-          <Footer>
-            <RestoreDefaultsButton
-              onClick={async () => await restoreDefaultCustomNetworks()}
-            >
-              <RestoreDefaultsButtonIcon>
-                <RefreshIcon fontSize="inherit" />
-              </RestoreDefaultsButtonIcon>
-              <div>Restore default networks</div>
-            </RestoreDefaultsButton>
-          </Footer>
+    <NavigationContainer
+      leftButton={<BarBackButton onClick={() => navigate(-1)} />}
+      title={"Networks"}
+    >
+      <CellStack>
+        {!allNetworks ? (
+          <Spinner />
+        ) : allNetworks.length === 0 ? (
+          <P>No custom networks found</P>
+        ) : (
+          allNetworks.map((network) => (
+            <DappConnection
+              key={network.id}
+              host={network.name}
+              onClick={() => {
+                setSelectedCustomNetwork(network)
+                navigate(routes.settingsEditCustomNetwork())
+              }}
+              hideRemove={network.readonly}
+              onRemoveClick={async () => {
+                await removeNetwork(network.id)
+              }}
+            />
+          ))
         )}
-      </Wrapper>
-    </>
+      </CellStack>
+
+      <Link to={routes.settingsAddCustomNetwork()}>
+        <IconButtonCenter size={48} style={{ marginTop: "32px" }}>
+          <AddIcon fontSize="large" />
+        </IconButtonCenter>
+      </Link>
+
+      {!isDefaultCustomNetworks && (
+        <Footer>
+          <RestoreDefaultsButton
+            onClick={async () => await restoreDefaultCustomNetworks()}
+          >
+            <RestoreDefaultsButtonIcon>
+              <RefreshIcon fontSize="inherit" />
+            </RestoreDefaultsButtonIcon>
+            <div>Restore default networks</div>
+          </RestoreDefaultsButton>
+        </Footer>
+      )}
+    </NavigationContainer>
   )
 }
