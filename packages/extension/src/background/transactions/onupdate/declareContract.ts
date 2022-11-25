ebecb0cdb4f078c03b5e2073dad1fffb36dea10e
bluecco <bluecco@gmail.com>
2022-11-25 15:10:20 +0100

    chore: refactor

--- packages/extension/src/background/transactions/onupdate/declareContract.ts
+++ packages/extension/src/background/transactions/onupdate/declareContract.ts
@@ -5,18 +5,12 @@ import { TransactionUpdateListener } from "./type"
 export const handleDeclareContractTransaction: TransactionUpdateListener =
   async (transactions) => {
     for (const transaction of transactions) {
-      if (
-        transaction.meta?.type === UdcTransactionType.DECLARE_CONTRACT &&
-        transaction.status !== "REJECTED"
-      ) {
-        await declaredTransactionsStore.push(transaction)
-      }
-
-      if (
-        transaction.meta?.type === UdcTransactionType.DECLARE_CONTRACT &&
-        transaction.status === "REJECTED"
-      ) {
-        await declaredTransactionsStore.remove(transaction)
+      if (transaction.meta?.type === UdcTransactionType.DECLARE_CONTRACT) {
+        if (transaction.status !== "REJECTED") {
+          await declaredTransactionsStore.push(transaction)
+        } else {
+          await declaredTransactionsStore.remove(transaction)
+        }
       }
     }
   }
