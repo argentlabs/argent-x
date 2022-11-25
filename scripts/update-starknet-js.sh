e36f09c7ede647740b58bd7c03185dd9ecd40413
bluecco <bluecco@gmail.com>
2022-11-25 13:20:23 +0100

    chore: update starknetjs scripts

--- scripts/update-starknet-js.sh
+++ scripts/update-starknet-js.sh
@@ -2,4 +2,5 @@
 yarn --cwd packages/extension add starknet@next
 yarn --cwd packages/dapp add starknet@next
 yarn --cwd packages/sessions add starknet@next
+yarn --cwd packages/multicall add starknet@next
 yarn --cwd packages/get-starknet add -D starknet@next
