ca68760db53d5c5bbf61744f1822589ad42a33d2
Janek <janek.rahrt@me.com>
2022-11-26 18:56:08 +0100

    feat: submitting state

--- packages/get-starknet/src/modal/Modal.svelte
+++ packages/get-starknet/src/modal/Modal.svelte
@@ -60,6 +60,8 @@
   })
   const mailForm = form(mail)
 
+  let mailSubmitting = false
+
   const wallets = [
     lastWallet,
     ...preAuthorizedWallets,
@@ -123,10 +125,15 @@
       <!-- webwallet -->
       <main>
         <form
-          class="relative"
+          class="relative group group-first:disabled:cursor-pointer"
           on:submit={async (e) => {
             e.preventDefault()
             await mailForm.validate()
+            if (!$mailForm.valid) {
+              return
+            }
+            mailSubmitting = true
+            // TODO: Submit email to backend
           }}
         >
           <!-- svelte-ignore a11y-autofocus -->
@@ -135,10 +142,12 @@
             class={"peer w-full mb-2 p-5 py-[18px] rounded-xl bg-neutral-100 dark:bg-black placeholder:text-neutral-500 placeholder:dark:text-neutral-400 text-base focus:outline-none transition-colors " +
               ($mail.invalid
                 ? "ring-2 ring-red-500 dark:ring-red-500"
-                : "focus:ring-2 focus:ring-neutral-200 dark:focus:ring-neutral-700")}
-            type="email"
+                : "focus:ring-2 focus:ring-neutral-200 dark:focus:ring-neutral-700") +
+              (mailSubmitting ? " opacity-50" : "")}
+            type="text"
             name="email"
             placeholder="Email"
+            disabled={mailSubmitting}
             bind:value={$mail.value}
             on:keyup={() => {
               if ($mail.invalid) {
@@ -157,29 +166,66 @@
             class={"absolute right-4 top-1/2 -translate-y-1/2 peer-placeholder-shown:opacity-0 peer-placeholder-shown:scale-0 opacity-1 scale-1 transition-all duration-300 " +
               ($mail.invalid ? "opacity-0 scale-0" : "")}
           >
-            <button
-              tabindex="0"
-              class="bg-neutral-200 dark:bg-neutral-800 text-neutral-400 dark:text-white rounded-full p-2 hover:bg-neutral-100 hover:disabled:bg-neutral-200 dark:hover:bg-neutral-700 dark:hover:disabled:bg-neutral-800 transition-colors cursor-pointer focus:outline-none focus:ring-2 focus:ring-neutral-200 dark:focus:ring-neutral-700"
-              type="submit"
-              disabled={!$mailForm.valid}
-            >
-              <svg
-                width="12"
-                height="12"
-                viewBox="0 0 12 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+            {#if mailSubmitting}
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  class="w-6 h-6 text-neutral-300 animate-spin dark:text-neutral-600 fill-neutral-600 dark:fill-neutral-300"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span class="sr-only">Loading...</span>
+              </div>
+            {:else}
+              <button
+                tabindex="0"
+                class="bg-neutral-200 dark:bg-neutral-800 text-neutral-400 dark:text-white rounded-full p-2 hover:bg-neutral-100 hover:disabled:bg-neutral-200 dark:hover:bg-neutral-700 dark:hover:disabled:bg-neutral-800 transition-colors cursor-pointer focus:outline-none focus:ring-2 focus:ring-neutral-200 dark:focus:ring-neutral-700"
+                type="submit"
+                disabled={!$mailForm.valid}
               >
-                <path
-                  d="M4.5 10.5L8.5 6.5L4.5 2.5"
-                  stroke="currentColor"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4.5 10.5L8.5 6.5L4.5 2.5"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+            {/if}
           </div>
+          {#if mailSubmitting}
+            <div
+              class="absolute rounded-xl left-0 right-0 bottom-0 top-0 cursor-pointer focus:ring-2 focus:outline-none focus:ring-neutral-200 dark:focus:ring-neutral-700"
+              role="button"
+              tabindex="0"
+              on:click={() => {
+                // TODO: Bring window to front
+              }}
+              on:keydown={(e) => {
+                if (e.key === "Enter") {
+                  // TODO: Bring window to front
+                }
+              }}
+            />
+          {/if}
         </form>
 
         <div class="flex items-center justify-between mb-8 px-2">
