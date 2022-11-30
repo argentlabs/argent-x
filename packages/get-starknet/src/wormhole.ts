c9b4938a3a5952573990c5dc79d16413a2138c1f
Janek <janek.rahrt@me.com>
2022-11-30 20:46:31 +0100

    fix: iframe inline styles

--- packages/get-starknet/src/wormhole.ts
+++ packages/get-starknet/src/wormhole.ts
@@ -3,94 +3,116 @@ import { getRemoteHandle } from "@argent/x-window"
 import retry from "async-retry"
 import memo from "lodash-es/memoize"
 
-export const getIframeConnection = memo(
-  async (targetUrl: string) =>
-    new Promise<RemoteConnection>((resolve, reject) => {
-      const iframe = document.createElement("iframe")
-      iframe.src = targetUrl
-      // middle of the screen
-      iframe.style.position = "fixed"
-      iframe.style.top = "50%"
-      iframe.style.left = "50%"
-      iframe.style.transform = "translate(-50%, -50%)"
-      iframe.style.width = "380px"
-      iframe.style.height = "420px"
-      iframe.style.border = "none"
-      // sandbox
-      iframe.sandbox.add(
-        "allow-scripts",
-        "allow-same-origin",
-        "allow-forms",
-        "allow-top-navigation",
-      )
-      // round corners
-      iframe.style.borderRadius = "40px"
-      // box shadow
-      iframe.style.boxShadow = "0px 4px 20px rgba(0, 0, 0, 0.5)"
+export const applyModalStyle = (iframe: HTMLIFrameElement) => {
+  // middle of the screen
+  iframe.style.position = "fixed"
+  iframe.style.top = "50%"
+  iframe.style.left = "50%"
+  iframe.style.transform = "translate(-50%, -50%)"
+  iframe.style.width = "380px"
+  iframe.style.height = "420px"
+  iframe.style.border = "none"
 
-      const background = document.createElement("div")
-      background.style.display = "none"
-      background.style.position = "fixed"
-      background.style.top = "0"
-      background.style.left = "0"
-      background.style.right = "0"
-      background.style.bottom = "0"
-      background.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
-      background.style.zIndex = "9999"
-      ;(background.style as any).backdropFilter = "blur(4px)"
+  // round corners
+  iframe.style.borderRadius = "40px"
+  // box shadow
+  iframe.style.boxShadow = "0px 4px 20px rgba(0, 0, 0, 0.5)"
 
-      setTimeout(() => reject(new Error("Timeout")), 20000)
+  const background = document.createElement("div")
+  background.style.display = "block"
+  background.style.position = "fixed"
+  background.style.top = "0"
+  background.style.left = "0"
+  background.style.right = "0"
+  background.style.bottom = "0"
+  background.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+  background.style.zIndex = "9999"
+  ;(background.style as any).backdropFilter = "blur(4px)"
 
-      const iframeLoaded = new Promise<void>((resolve) => {
-        iframe.addEventListener("load", () => {
-          resolve()
-        })
-      })
+  background.appendChild(iframe)
 
-      background.appendChild(iframe)
-      document.body.appendChild(background)
+  return background
+}
 
-      iframeLoaded.then(async () => {
-        const handle = await retry(
-          () =>
-            getRemoteHandle({
-              remoteWindow: iframe.contentWindow,
-              remoteOrigin: "*",
-              localWindow: window,
-            }),
-          {
-            maxRetryTime: 5,
-            minTimeout: 500,
-          },
-        ).catch((cause) => {
-          throw Error("Failed to connect to iframe", { cause })
-        })
+export const applyInlineStyle = (iframe: HTMLIFrameElement) => {
+  iframe.style.width = "100%"
+  iframe.style.height = "104px"
+  iframe.style.border = "none"
+  iframe.style.borderRadius = "0px"
+  iframe.style.boxShadow = "none"
+  iframe.style.position = "relative"
+  iframe.style.top = "0"
+  iframe.style.left = "0"
+  iframe.style.transform = "none"
+  iframe.style.display = "block"
 
-        handle.addEventListener("ARGENT_WEB_WALLET::SHOULD_SHOW", () => {
-          background.style.display = "block"
-        })
-        handle.addEventListener("ARGENT_WEB_WALLET::SHOULD_HIDE", () => {
-          background.style.display = "none"
-        })
-        handle.addEventListener(
-          "ARGENT_WEB_WALLET::HEIGHT_CHANGED",
-          (height) => {
-            iframe.style.height = `min(${height || 420}px, 100%)`
-          },
-        )
+  return iframe
+}
 
-        await handle.once("ARGENT_WEB_WALLET::LOADED")
+export const applyHideStyle = (iframe: HTMLIFrameElement) => {
+  iframe.style.display = "none"
+  iframe.style.position = "absolute"
+  iframe.style.top = "-10000px"
+  iframe.style.left = "-10000px"
+  iframe.style.maxWidth = "1000px"
+  iframe.style.maxHeight = "1000px"
+  iframe.style.border = "none"
+  iframe.style.borderRadius = "0px"
+  iframe.style.boxShadow = "none"
+  iframe.style.transform = "none"
+}
 
-        return resolve(handle)
-      })
-    }),
-)
+export const createIframe = async (targetUrl: string) => {
+  const iframe = document.createElement("iframe")
+  iframe.src = targetUrl
+  applyHideStyle(iframe)
+  ;(iframe as any).loading = "eager"
+  iframe.sandbox.add(
+    "allow-scripts",
+    "allow-same-origin",
+    "allow-forms",
+    "allow-top-navigation",
+  )
 
-export const wormhole = (targetUrl: string) =>
-  getIframeConnection(targetUrl).then((h) => {
-    console.log("wormhole", h)
-    return h
+  return iframe
+}
+
+export const loadIframe = async (iframe: HTMLIFrameElement) =>
+  new Promise<HTMLIFrameElement>((resolve, reject) => {
+    const pid = setTimeout(
+      () => reject(new Error("Timeout while opening an iframe")),
+      20000,
+    )
+
+    iframe.addEventListener("load", async () => {
+      clearTimeout(pid)
+      await new Promise((resolve) => setTimeout(resolve, 500)) // wait for all animations in the iframe to finish
+      resolve(iframe)
+    })
   })
+
+export const getIframeConnection = memo(
+  async (iframe: HTMLIFrameElement): Promise<RemoteConnection> => {
+    const handle = await retry(
+      () =>
+        getRemoteHandle({
+          remoteWindow: iframe.contentWindow,
+          remoteOrigin: "*",
+          localWindow: window,
+        }),
+      {
+        maxRetryTime: 5,
+        minTimeout: 500,
+      },
+    ).catch((cause) => {
+      throw Error("Failed to connect to iframe", { cause })
+    })
+
+    await handle.once("ARGENT_WEB_WALLET::LOADED")
+
+    return handle
+  },
+)
 
 export const getMemorizedLoginStatus = memo(
   async (wormholeConnection: RemoteConnection) =>
