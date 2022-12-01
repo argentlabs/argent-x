5288bab5864eda4fde176d4beba76cd4bd019b9b
Janek <janek.rahrt@me.com>
2022-12-01 19:50:41 +0100

    fix: dapp in prod

--- packages/dapp/next.config.js
+++ packages/dapp/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false, // we need to use terser, as swc doesn't support svelte and `@argent/get-starknet`
 }
