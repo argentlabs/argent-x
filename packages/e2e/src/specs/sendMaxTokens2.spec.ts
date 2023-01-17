a67801f4e0cab7a1cc57755dfe54850d1b7da009
César Lopes <cesarlopes@Cesars-Laptop.local>
2023-01-17 10:29:31 +0000

    add test

---
+++ packages/e2e/src/specs/sendMaxTokens2.spec.ts
@@ -0,0 +1,78 @@
+import { expect } from "@playwright/test"
+
+import { extension, test } from "../test"
+
+test.describe("Send max tokens", () => {
+  test("send max eth flow", async () => {
+    // disable page transitions so we don't need to wait for elements to settle
+    await extension.page.emulateMedia({ reducedMotion: "reduce" })
+    await extension.wallet.newWalletOnboarding()
+    await extension.open()
+    await expect(extension.network.networkSelector).toBeVisible()
+    await extension.network.selectNetwork("Localhost 5050")
+    const [accountName1] = await extension.account.addAccount({})
+    const [accountName2, accountAddress2] = await extension.account.addAccount({
+      firstAccount: false,
+    })
+    if (!accountName1 || !accountName2 || !accountAddress2) {
+      throw new Error("Invalid account names")
+    }
+    const assetsAccount2 = await extension.account.assets(accountName2)
+    const assetsAccount1 = await extension.account.assets(accountName1)
+    await Promise.all([
+      expect(assetsAccount1.find((el) => (el.name = "Ethereum"))?.balance).toBe(
+        1,
+      ),
+      expect(assetsAccount2.find((el) => (el.name = "Ethereum"))?.balance).toBe(
+        1,
+      ),
+    ])
+
+    await extension.account.transfer({
+      originAccountName: accountName1,
+      recepientAddress: accountAddress2,
+      tokenName: "Ethereum",
+      ammount: "MAX",
+    })
+
+    await extension.account.token("Ethereum").click()
+    await expect(extension.account.balance).toContainText("0.00")
+    await extension.account.back.click()
+    await extension.account.ensureAccount(accountName2)
+    await extension.account.token("Ethereum").click()
+    await expect(extension.account.balance).not.toContainText("0.00")
+    await extension.account.back.click()
+
+    for (let idx = 2; idx < 32; idx++) {
+      const [accountName, accountAddress] = await extension.account.addAccount({
+        firstAccount: false,
+      })
+      console.log(accountName, accountAddress)
+
+      if (accountName && accountAddress) {
+        await extension.account.transfer({
+          originAccountName: accountName,
+          recepientAddress: accountAddress,
+          tokenName: "Ethereum",
+          ammount: "MAX",
+        })
+      }
+
+      // await extension.account.back.click()
+    }
+    //@TODO https://argent.atlassian.net/browse/BLO-670
+    /*
+        const updatedAssetsAccoun1 = await extension.account.assets(accountName1)
+        const updatedAssetsAccoun2 = await extension.account.assets(accountName2)
+    
+        await Promise.all([
+          expect(
+            updatedAssetsAccoun1.find((el) => (el.name = "Ethereum"))?.balance,
+          ).toContain(0.00),
+          expect(
+            updatedAssetsAccoun2.find((el) => (el.name = "Ethereum"))?.balance,
+          ).toContain(1.9),
+        ])
+      */
+  })
+})
