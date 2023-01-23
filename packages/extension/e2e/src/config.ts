119713cd17c911fcfd3b9bdca6e14103ee82b5a8
Cesar <cesar@argent.xyz>
2023-01-23 09:46:39 +0000

    move tests

--- packages/extension/e2e/src/config.ts
+++ packages/extension/e2e/src/config.ts
@@ -3,5 +3,5 @@ import path from "path"
 export default {
   artifactsDir: path.resolve(__dirname, "../artifacts/playwright"),
   reportsDir: path.resolve(__dirname, "../artifacts/reports"),
-  distDir: path.join(__dirname, "../../extension/dist/"),
+  distDir: path.join(__dirname, "../../dist/"),
 }
--- packages/extension/e2e/steps/navigateBack.ts
+++
@@ -1,5 +0,0 @@
-import type { Page } from "@playwright/test"
-
-export async function navigateBack(page: Page) {
-  await page.click("[aria-label='Back']")
-}
