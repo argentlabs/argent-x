ac7c3b313ebbe09e493c4c2b186a06a6a0090464
diego delrieu <diegodelrieu@diegos-MacBook-Pro.local>
2023-02-02 15:18:50 +0100

    improve error handling

--- packages/extension/src/shared/messages/RecoveryMessage.ts
+++ packages/extension/src/shared/messages/RecoveryMessage.ts
@@ -4,6 +4,6 @@ export type RecoveryMessage =
   | { type: "RECOVER_BACKUP_REJ"; data: string }
   | { type: "RECOVER_SEEDPHRASE"; data: { secure: true; body: string } }
   | { type: "RECOVER_SEEDPHRASE_RES" }
-  | { type: "RECOVER_SEEDPHRASE_REJ" }
+  | { type: "RECOVER_SEEDPHRASE_REJ"; data: string }
   | { type: "DOWNLOAD_BACKUP_FILE" }
   | { type: "DOWNLOAD_BACKUP_FILE_RES" }
