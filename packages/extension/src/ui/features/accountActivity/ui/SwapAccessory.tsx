0031eada9a258e6dfbd25beee397ff0f31a43046
diegodelrieue <diego@argent.xyz>
2023-03-01 15:53:56 +0100

    implement component

--- packages/extension/src/ui/features/accountActivity/ui/SwapAccessory.tsx
+++ packages/extension/src/ui/features/accountActivity/ui/SwapAccessory.tsx
@@ -1,4 +1,4 @@
-import { H6, P4 } from "@argent/ui"
+import { H6, P4, TextWithAmount } from "@argent/ui"
 import { Flex } from "@chakra-ui/react"
 import { FC } from "react"
 
@@ -13,41 +13,45 @@ export const SwapAccessory: FC<SwapAccessoryProps> = ({ transaction }) => {
   const { fromAmount, fromToken, toAmount, toToken } = transaction
   return (
     <Flex direction={"column"} overflow="hidden">
-      <H6
-        overflow="hidden"
-        textOverflow={"ellipsis"}
-        textAlign={"right"}
-        color={"secondary.500"}
-      >
-        <>+</>
-        {toToken ? (
-          prettifyTokenAmount({
-            amount: toAmount,
-            decimals: toToken.decimals,
-            symbol: toToken.symbol,
-          })
-        ) : (
-          <>{toAmount} Unknown</>
-        )}
-      </H6>
-      <P4
-        color="neutrals.400"
-        fontWeight={"semibold"}
-        overflow="hidden"
-        textOverflow={"ellipsis"}
-        textAlign={"right"}
-      >
-        &minus;
-        {fromToken ? (
-          prettifyTokenAmount({
-            amount: fromAmount,
-            decimals: fromToken.decimals,
-            symbol: fromToken.symbol,
-          })
-        ) : (
-          <>{fromAmount} Unknown</>
-        )}
-      </P4>
+      <TextWithAmount amount={toAmount}>
+        <H6
+          overflow="hidden"
+          textOverflow={"ellipsis"}
+          textAlign={"right"}
+          color={"secondary.500"}
+        >
+          <>+</>
+          {toToken ? (
+            prettifyTokenAmount({
+              amount: toAmount,
+              decimals: toToken.decimals,
+              symbol: toToken.symbol,
+            })
+          ) : (
+            <>{toAmount} Unknown</>
+          )}
+        </H6>
+      </TextWithAmount>
+      <TextWithAmount amount={fromAmount}>
+        <P4
+          color="neutrals.400"
+          fontWeight={"semibold"}
+          overflow="hidden"
+          textOverflow={"ellipsis"}
+          textAlign={"right"}
+        >
+          &minus;
+          {fromToken ? (
+            prettifyTokenAmount({
+              amount: fromAmount,
+              decimals: fromToken.decimals,
+              symbol: fromToken.symbol,
+            })
+          ) : (
+            <>{fromAmount} Unknown</>
+          )}
+        </P4>
+      </TextWithAmount>
     </Flex>
   )
 }
