0250567f080e773689ceb2a9afdd795464f89932
diegodelrieue <diego@argent.xyz>
2023-03-01 15:53:30 +0100

    setup ui test

---
+++ packages/ui/test/setup.ts
@@ -0,0 +1,20 @@
+import "@testing-library/jest-dom/extend-expect"
+
+import fetch from "cross-fetch"
+import { noop } from "lodash-es"
+import { vi } from "vitest"
+
+vi.stubGlobal("fetch", fetch)
+vi.stubGlobal("chrome", {
+  runtime: {
+    id: "test",
+  },
+  alarms: {
+    create: noop,
+    set: noop,
+    get: (_: unknown, cb: (result: null) => void) => cb(null),
+    onAlarm: {
+      addListener: noop,
+    },
+  },
+})
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,8 +1,8 @@
 import { resolve as resolvePath } from "path"
 
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [
@@ -11,6 +11,33 @@ export default defineConfig({
     }),
     react(),
   ],
+  test: {
+    globals: true,
+    setupFiles: "test/setup.ts",
+    environment: "jsdom",
+    testTimeout: 50 * 60 * 1000,
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/e2e/**",
+      "**/.{idea,git,cache,output,temp}/**",
+    ],
+    coverage: {
+      exclude: [
+        "**/*.mock.ts",
+        "**/setup.ts",
+        "**/*.json",
+        "**/*.config.{js,ts}",
+        "test{,s}/**",
+        "test{,-*}.{js,cjs,mjs,ts,tsx,jsx}",
+        "**/*.d.ts",
+      ],
+      reportsDirectory: "./coverage",
+      excludeNodeModules: true,
+      reporter: ["text", "lcov"],
+      all: true,
+    },
+  },
   build: {
     lib: {
       entry: resolvePath(__dirname, "src/index.ts"),
