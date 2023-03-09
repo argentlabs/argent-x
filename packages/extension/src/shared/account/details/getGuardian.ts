71f71d46d05487d1f2db0e248cd4f6c91081b848
Simon Heys <si@simonheys.com>
2023-03-09 16:51:04 +0000

    feat: check latest implementations for 2fa

--- packages/extension/src/shared/account/details/getGuardian.ts
+++ packages/extension/src/shared/account/details/getGuardian.ts
@@ -3,14 +3,22 @@ import { Call, constants, number, uint256 } from "starknet"
 import { getMulticallForNetwork } from "../../multicall"
 import { getNetwork } from "../../network"
 import { BaseWalletAccount } from "../../wallet.model"
+import { getIsCurrentImplementation } from "./getImplementation"
 
 /**
- * Get guardian address of account, or undefined if getGuardian returns `0x0`
+ * Get guardian address of account, or undefined if getGuardian returns `0x0` or account is not current implementation
  */
 
 export const getGuardianForAccount = async (
   account: BaseWalletAccount,
 ): Promise<string | undefined> => {
+  /**
+   * Skip older implementations which may use 'get_guardian'
+   */
+  const isCurrent = await getIsCurrentImplementation(account)
+  if (!isCurrent) {
+    return
+  }
   const network = await getNetwork(account.networkId)
   const call: Call = {
     contractAddress: account.address,
